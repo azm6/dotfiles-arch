e8c93dd7d20ab9c7f3365743ab71dfda8d447117
azme <bazme99@gmail.com>
2024-03-26 08:04:48 +0100

    Add theme update

--- nvim/.config/nvim/lua/config/packer.lua
+++ nvim/.config/nvim/lua/config/packer.lua
@@ -12,7 +12,8 @@ return require('packer').startup(function(use)
 		requires = { {'nvim-lua/plenary.nvim'} }
 
 	})
-	use ('navarasu/onedark.nvim')
+	-- use ('navarasu/onedark.nvim')
+    use { "catppuccin/nvim", as = "catppuccin" }
 	use ( 'nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'}
 	)
 	use ('theprimeagen/harpoon')
